818b4bcde0d04f8161fd584f0dd61f8c74eb2d19
Paulo Vidal <u1d4lp@gmail.com>
2022-10-20 18:58:09 -0300

    refactor: mod org

--- eip712-spec/lib.rs
+++ eip712-spec/lib.rs
@@ -1,12 +1,13 @@
 //! # EIP-712
 //! This crate provides a simple implementation of the [EIP-712](https://eips.ethereum.org/EIPS/eip-712) specification.
 
-pub use primitive_types::{H160, H256, U256};
 pub use parity_crypto::Keccak256;
+pub use primitive_types::{H160, H256, U256};
 
 pub mod domain;
 pub mod encode;
 pub mod s;
+pub mod sol;
 
 pub use domain::*;
 pub use s::*;
